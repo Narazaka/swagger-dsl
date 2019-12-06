029984104888a2762af3486c440741452f250a9a
Narazaka <info@narazaka.net>
2019-12-07 06:21:40 +0900

    fix spec

--- spec/swagger/dsl_spec.rb
+++ spec/swagger/dsl_spec.rb
@@ -4,46 +4,51 @@ require_relative "./rails_fixture"
 RSpec.describe Swagger::DSL do
   subject { Swagger::DSL.current }
 
+  let(:patch) do
+    {
+      "operationId" => "UsersController#update",
+      "parameters" => [
+        { "name" => :id, "schema" => { "type" => :integer }, "required" => true, "in" => :path },
+        { "name" => :safe, "schema" => { "type" => :boolean }, "in" => :query },
+        {
+          "name" => :redirect,
+          "in" => :query,
+          "required" => true,
+          "schema" => { "type" => "string", "format" => "url" },
+        },
+      ],
+      "requestBody" => {
+        "required" => true,
+        "content" => { "application/json" => { "schema" => { "$ref" => "#/components/User" } } },
+      },
+      "responses" => {
+        200 => {
+          "content" => {
+            "application/json" => {
+              "schema" => {
+                "type" => "object",
+                "properties" => {
+                  "status" => { "enum" => %w[ok], "default" => :ok, "type" => "string" },
+                  "user" => { "$ref" => "#/components/User" },
+                },
+                "required" => %w[status user],
+                "additionalProperties" => false,
+              },
+            },
+          },
+        },
+      },
+    }
+  end
+
   let(:schema) do
     {
       "openapi" => "3.0.0",
       "info" => { "title" => "my app", "version" => "0.1.0" },
       "paths" => {
         "/users/{id}" => {
-          "patch" => {
-            "operationId" => "UsersController#update",
-            "parameters" => [
-              { "name" => :id, "schema" => { "type" => :integer }, "required" => true, "in" => :path },
-              { "name" => :safe, "schema" => { "type" => :boolean }, "in" => :query },
-              {
-                "name" => :redirect,
-                "in" => :query,
-                "required" => true,
-                "schema" => { "type" => "string", "format" => "url" },
-              },
-            ],
-            "requestBody" => {
-              "required" => true,
-              "content" => { "application/json" => { "schema" => { "$ref" => "#/components/User" } } },
-            },
-            "responses" => {
-              200 => {
-                "content" => {
-                  "application/json" => {
-                    "schema" => {
-                      "type" => "object",
-                      "properties" => {
-                        "status" => { "enum" => %w[ok], "default" => :ok, "type" => "string" },
-                        "user" => { "$ref" => "#/components/User" },
-                      },
-                      "required" => %w[status user],
-                      "additionalProperties" => false,
-                    },
-                  },
-                },
-              },
-            },
-          },
+          "patch" => patch,
+          "put" => patch,
         },
       },
       "components" => {
